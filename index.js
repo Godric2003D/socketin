d90482240213f0a0fff84c3bc2c6d29fdc3640e6
Godric2003D <66315809+Godric2003D@users.noreply.github.com>
2022-04-15 16:24:53 +0530

    wf

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io")
 const io = new Server(server);
+const port = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -21,6 +23,6 @@ io.on('connection', (socket) => {
         io.emit('chat message', msg);
     });
   });
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('Server is Up !  listening on *:3000');
 });
